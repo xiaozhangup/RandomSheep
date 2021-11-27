61990e21e86bf7a3291a899b945729bdfef3df64
xiaozhangup <xiaozhangshiw@163.com>
2021-11-27 21:19:11 +0800

    Update

---
+++ src/main/java/me/xiaozhangup/randomsheep/Enums.java
@@ -0,0 +1,16 @@
+package me.xiaozhangup.randomsheep;
+
+import java.util.Random;
+
+public class Enums {
+    private static int random=(int)(Math.random()*10);
+    private static Random rand = new Random(random);
+
+    public static <T extends Enum<T>> T random(Class<T> ec) {
+        return random(ec.getEnumConstants());
+    }
+
+    public static <T> T random(T[] values) {
+        return values[rand.nextInt(values.length)];
+    }
+}
--- src/main/java/me/xiaozhangup/randomsheep/Main.java
+++ src/main/java/me/xiaozhangup/randomsheep/Main.java
@@ -1,12 +1,18 @@
 package me.xiaozhangup.randomsheep;
 
 import org.bukkit.Bukkit;
+import org.bukkit.DyeColor;
 import org.bukkit.Location;
+import org.bukkit.World;
+import org.bukkit.entity.Entity;
 import org.bukkit.entity.EntityType;
+import org.bukkit.entity.Sheep;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.entity.EntityDamageEvent;
 import org.bukkit.plugin.java.JavaPlugin;
+
+import java.util.Random;
 
 public class Main extends JavaPlugin {
 
@@ -20,8 +26,23 @@ public class Main extends JavaPlugin {
                     Location loc = e.getEntity().getLocation();
                     e.getEntity().remove();
                     loc.createExplosion(1 , false , false);
+                    createSheep(e.getEntity().getWorld());
                 }
             }
         } , this);
+    }
+
+    public static void createSheep(World world) {
+        Location loc = world.getSpawnLocation();
+        loc.setX(loc.getX() + getRandom(-3 , 3));
+        loc.setZ(loc.getZ() + getRandom(-3 , 3));
+
+        Sheep sheep = (Sheep) world.spawnEntity(loc , EntityType.SHEEP);
+        sheep.setAdult();
+        sheep.setColor(Enums.random(DyeColor.class));
+    }
+
+    public static int getRandom(int start, int end) {
+        return (int)(Math.random() * (end - start + 1) + start);
     }
 }
